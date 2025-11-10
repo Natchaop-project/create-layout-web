bb9e416d80c35b26e1c029a76a71b6cd2ec9b8f4
Natchanop <domenatchanop@gmail.com>
2025-11-11 03:03:27 +0700

    add link

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,11 @@
 // src/routes/AppRoutes.jsx
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Links } from "react-router-dom";
 import CreatePage from '../pages/CreatePage.jsx';
 
 export default function AppRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<h4>หน้า การขาย</h4>} />
+            <Route path="/" element={<Links to="/sale/deposit/edit">แก้ไขเอกสาร</Links> } />
             <Route path="/sale" element={<h4>หน้า ข้อมูลการขาย</h4>} />
             <Route path="/sale/deposit" element={<h4>หน้า รับมัดจำ</h4>} />
             <Route path="/sale/deposit/edit" element={<CreatePage />} />
